ba8599d3f99a4f0cc7f47088d484b72d86d43979
Tyjoo26 <Tyjoo21@gmail.com>
2017-07-08 18:03:58 -0600

    Add methods convert integer, square integer, push integer into array

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,9 +6,21 @@ class Enigma
     @date = Time.now.strftime('%m/%d/%y')
   end
 
-  def date_convert_integer
-    Time.now.strftime('%m/%d/%y').to_i
+  def remove_backslash
+    date_string = Time.now.strftime('%m/%d/%y').gsub('/',"")
   end
 
 
+  def date_convert_integer
+    date_integer = Time.now.strftime('%m%d%y').to_i
+
+  end
+
+  def date_integer_squared(date_integer)
+    squared_date = (date_integer ** 2)
+  end
+
+  def squared_date_array(squared_date)
+    array = [] << squared_date
+  end
 end
